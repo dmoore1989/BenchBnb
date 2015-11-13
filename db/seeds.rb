77239126dc944f233a7e760ca77126810818f5b8
Douglas Moore <douglas.moore89@gmail.com>
2015-11-13 10:16:59 -0500

    Create rails backbone/seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+benches = Bench.create([
+  {
+  description:"Near the 4th Ave-9th St stop",
+  lat:40.670355,
+  lng: -73.988935
+},
+  {
+  description: "Cozy Prospect Park Location",
+  lat: 40.661205,
+  lng: -73.979700
+},
+  {
+  description: "Hip trendy Gowanus bench, close to Whole Foods!!!!",
+  lat: 40.676945,
+  lng: -73.991759
+},
+  {
+  description: "If you love superfund sites, you'll love this bench!!!!",
+  lat: 40.673995,
+  lng: -73.996031
+},
+  {
+  description: "A dunkin donuts and checkers are just a few feet away",
+  lat: 40.668368,
+  lng: -73.993751
+},
+  ])
