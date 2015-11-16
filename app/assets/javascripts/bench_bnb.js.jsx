3513968410bd9c1296c39839a2b1de1dfe7b138c
Douglas Moore <douglas.moore89@gmail.com>
2015-11-16 09:22:26 -0500

    Add React Router

--- app/assets/javascripts/bench_bnb.js.jsx
+++ app/assets/javascripts/bench_bnb.js.jsx
@@ -1,6 +1,25 @@
 $(function (){
+  var Router = ReactRouter.Router;
+  var Route = ReactRouter.Route;
+  var IndexRoute = ReactRouter.IndexRoute;
+
+  var App = React.createClass({
+    render: function () {
+      return (
+        <div>
+          <header><h1>Bench Bnb</h1></header>
+          {this.props.children}
+        </div>
+      );
+    }
+  });
+
   React.render(
-    <Search />,
+    <Router>
+      <Route path="/" component={App}>
+        <IndexRoute component={Search} />
+      </Route>
+    </Router>,
     document.getElementById("content")
   );
 
